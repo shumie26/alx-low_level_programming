6f54c5ca29f0b0b1d14eb8a9f143b7ad028368d4
Shumirai Tarugarira <smanjengwah@gmail.com>
2023-02-16 06:51:54 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - print a line of code using puts
+ *
+ * Return: 0
+ */
+Int main (void)
+{  
+	Puts ("\"Programming is like building a multilingual puzzle");
+ 	return (0);
+}	
