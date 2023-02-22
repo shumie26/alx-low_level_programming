35f54421aebe8d4ebd3f007d2ff2b3e86a7cea2c
Shumirai Tarugarira <smanjengwah@gmail.com>
2023-02-22 03:59:25 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 /**
  * main - Print _putchar
  *
- * Return: 0
+ * Return: Always 0
  */
 
 int main(void)
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print alphabet- print alphabet in lowercase
+ */
+void print_alphabet(void);
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
